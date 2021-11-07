59dcf101b3b5d2f463af11a5c6c478240cef5f82
Yiyang-Chen <42653634+Yiyang-Chen@users.noreply.github.com>
2021-11-06 17:37:00 -0700

    more UI needed for battle

--- Assets/Scripts/BattleHUD.cs
+++ Assets/Scripts/BattleHUD.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleHUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Text nameText;
+    public Text weaponText;
+    public Slider weaponSlider;
+    public Text armorText;
+    public Slider armorSlider;
 }
